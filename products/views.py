7e93c5663f44c8b6b7ac719a10b660153ce46ab6
paulovsantos <paulo.vitors@hotmail.com>
2024-11-08 15:11:13 -0300

    Implementado o salvameto dos dados no BD usando form.

--- products/views.py
+++ products/views.py
@@ -1,15 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from products.models import Product
 
+def add_product(request):
+# Pegando os dados do formulário
+    new_product = Product()
+    new_product.title = request.POST.get('title')
+    new_product.category = request.POST.get('category')
+    new_product.brand = request.POST.get('brand')
+    new_product.description = request.POST.get('description')
+    new_product.price = request.POST.get('price')
+   
+    new_product.save()
+    
+    return redirect('product_view')  # Redireciona para a view que exibe os produtos
 
-# Create your views here.
-
+                 
+# Exibe os dados dos produtos cadastrados.
 def product_view(request):
-    products = Product.objects.all()
-    #return HttpResponse('Hello world!')
+    products = Product.objects.all()  # Busca todos os produtos
     return render(
         request=request,
         template_name='products.html',
         context={'products': products},
-    )
-
+     )
